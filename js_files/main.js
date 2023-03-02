28293552677107fa0907a117e246bea3b0a7f35b
arpankesh <arpankesh1999@gmail.com>
2023-03-02 07:12:54 +0530

    Added transition to 1st lift faulty logic

--- js_files/main.js
+++ js_files/main.js
@@ -5,7 +5,6 @@ generateBtn.addEventListener("click", () => {
     if (!Number.isInteger(numFloors) || !Number.isInteger(numLifts)) {
         alert("Please enter an integer value for both the number of floors and lifts");
     } else {
-        console.log("Hello");
         const floorContainer = document.getElementById("floor-container");
         const btnContainer = document.getElementById("btn-container");
         for (let f = 1; f <= numFloors; f++) {
@@ -16,21 +15,62 @@ generateBtn.addEventListener("click", () => {
             upBtn.innerHTML = "UP";
             upBtn.type = "button";
             upBtn.classList.add("btn", "btn-success", "btn-sm");
+            upBtn.setAttribute("id", `upBtn${numFloors - f + 1}`);
 
             const downBtn = document.createElement("button");
             downBtn.innerHTML = "DOWN";
             downBtn.type = "button";
             downBtn.classList.add("btn", "btn-danger", "btn-sm");
+            downBtn.setAttribute("id", `downBtn${numFloors - f + 1}`);
 
             btnsDiv.setAttribute("id", "floorBtnDiv");
-            btnsDiv.appendChild(upBtn);
-            btnsDiv.appendChild(downBtn);
+            if (f !== 1) {
+                btnsDiv.appendChild(upBtn);
+            }
+            if (f !== numFloors) {
+                btnsDiv.appendChild(downBtn);
+            }
             btnContainer.appendChild(btnsDiv);
         }
         for (let f = 1; f <= numFloors; f++) {
             const floorElemnet = document.createElement("div");
-            floorElemnet.setAttribute("id", "floor");
+            floorElemnet.setAttribute("id", `floor${numFloors - f + 1}`);
+            floorElemnet.classList.add("floor");
             floorContainer.appendChild(floorElemnet);
         }
+
+        for (let l = 1; l <= numLifts; l++) {
+            console.log("Hello");
+            const liftElement = document.createElement("div");
+            liftElement.setAttribute("id", `lift${l}`);
+            liftElement.classList.add("lift");
+            liftElement.style.left = `${(l - 1) * 120 + 20}px`;
+
+            const leftDoor = document.createElement("div");
+            const rightDoor = document.createElement("div");
+            leftDoor.setAttribute("id", `leftDoor${l}`);
+            rightDoor.setAttribute("id", `rightDoor${l}`);
+            leftDoor.classList.add("leftDoor");
+            rightDoor.classList.add("rightDoor");
+
+            liftElement.appendChild(leftDoor);
+            liftElement.appendChild(rightDoor);
+
+            const floor1 = document.getElementById("floor1");
+            floor1.appendChild(liftElement);
+        }
     }
-})
+    const lift1 = document.getElementById("lift1");
+    console.log(lift1.parentElement.previousSibling);
+
+    const upBtn1 = document.getElementById("upBtn1");
+    console.log(upBtn1);
+    upBtn1.addEventListener("click", () => {
+        console.log("Hi");
+        lift1.parentElement = lift1.parentElement.previousSibling;
+        console.log(lift1.parentElement);
+        lift1.style.transform = "translate3d(0,-110px, 0)";
+    })
+});
+
+
